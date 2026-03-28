7364da3879bfd2604095c6095402c0e8e9857a81
Alejandra Torres <agtorre3@yahoo.com>
2026-03-28 14:49:25 -0700

    Add render blueprint for deploy, add seeds to render script so it has admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 # This file should ensure the existence of records required to run the application in every environment (development,
 # test). The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 
-admin_email = ENV.fetch("SEED_ADMIN_EMAIL", "admin@bloomcoffee.local")
-admin_password = ENV.fetch("SEED_ADMIN_PASSWORD", "changeme123456")
+admin_email = ENV.fetch("SEED_ADMIN_EMAIL", "admin@bloomcoffee.com")
+admin_password = ENV.fetch("SEED_ADMIN_PASSWORD", "INeedCaffeine!123")
 
 User.find_or_create_by!(email: admin_email) do |user|
   user.password = admin_password
